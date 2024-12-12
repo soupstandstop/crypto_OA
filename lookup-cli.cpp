010ac558a286642e3699c69954976cc1c01bdb56
Helen <h42318@gmail.com>
2024-12-12 17:31:11 +0800

    add class include read-yaml & process-into-map functions.

--- lookup-cli.cpp
+++ lookup-cli.cpp
@@ -2,27 +2,40 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
-    YAML::Node node = YAML::LoadFile("./test.yaml");
-    unordered_map<string,unordered_map<string,string>> pplInfo; // {"Alice":{"age":"18","occupation":"student"},"Bob":{...}}
-    if (node.IsSequence()){
-        for (int i=0; i<node.size(); ++i){
-            unordered_map<string,string> subMap = node[i].as<unordered_map<string,string>>(); // extract map from vector element
-            if (subMap.find("name")==subMap.end()) continue; // if no name exists, ignore this subMap
-            string pplName = subMap["name"];
-            pplInfo[pplName] = subMap; // key=person name, value=personal info
-            pplInfo[pplName].erase("name"); // remove "person name" from personal info
+class YamlUtil{
+    private:
+        unordered_map<string,unordered_map<string,string>> pplInfo; // {"Alice":{"age":"18","occupation":"student"},"Bob":{...}}
+        YAML::Node node;
+        unordered_map<string,string> subMap;
+    public:
+        YamlUtil(const string& path){
+            node = YAML::LoadFile(path);
+            buildPplInfo();
         }
-    }
-    // for (auto ppl=pplInfo.begin(); ppl!=pplInfo.end(); ++ppl){
-    //     cout << ppl->first << ":" << endl;
-    //     for (auto info=ppl->second.begin(); info!=ppl->second.end(); ++info){
-    //         cout << info->first << ": " << info->second << ", ";
-    //     }
-    //     cout << endl;
-    // }
-    // string inputName = "Bob";
-    // string field = "occupation";
-    // cout << pplInfo[inputName][field] << endl;
+        bool ifSequence(){
+            return node.IsSequence();
+        }
+        void convertToSubMap(YAML::const_iterator it){
+            subMap.clear();
+            subMap = it->as<unordered_map<string,string>>();
+        }
+        void buildPplInfo(){
+            if (ifSequence()){
+                for (YAML::const_iterator it=node.begin(); it!=node.end(); ++it){
+                    convertToSubMap(it);
+                    if (subMap.find("name")==subMap.end()) continue; // if no name exists, ignore this subMap
+                    string pplName = subMap["name"];
+                    pplInfo[pplName] = subMap; // key=person name, value=personal info
+                    pplInfo[pplName].erase("name"); // remove "person name" from personal info
+                }
+            }
+        }
+        void lookup(string name, string field){
+            cout << pplInfo[name][field] << endl;
+        }
+};
+int main(){
+    YamlUtil yamlUtil("./test.yaml");
+    yamlUtil.lookup("Alice","age");
     return 0;
 }
